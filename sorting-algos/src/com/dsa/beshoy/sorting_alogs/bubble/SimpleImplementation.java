c512ff22cc12033d12066c7fa474ec42dcd71c57
Beshoy Farag <beshoynady401@gmail.com>
2026-03-31 18:14:52 +0200

    add bubble sort practice

---
+++ sorting-algos/src/com/dsa/beshoy/sorting_alogs/bubble/SimpleImplementation.java
@@ -0,0 +1,34 @@
+package com.dsa.beshoy.sorting_alogs.bubble;
+
+public class SimpleImplementation {
+    public static void main (String [] args){
+        int [] arr = {5,24,632,23,612};
+
+        bubbleSort(arr);
+
+    }
+    public static void bubbleSort(int [] arr){
+        int temp = 0;
+        boolean flag = false;
+        for(int i =0; i < arr.length- 1; i++){
+            for(int j = 0; j < arr.length - 1 - i; j++){
+
+                if(arr[j] > arr[j + 1]){
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    flag = true;
+                }
+            }
+
+            if(!flag){
+                break;
+            }
+        }
+
+        for (int num : arr){
+            System.out.println(num + " ");
+        }
+
+    }
+}
